5fb6b3a2ac800cb73f18716b881903e960d4138d
Humberto-hub206 <jhumbertocgomes@gmail.com>
2026-05-25 21:17:25 -0300

    Refactor file listing functionality with new methods

--- LIstadorArquivos.py
+++ LIstadorArquivos.py
@@ -1,36 +1,51 @@
 import os
 
-def listarArquivos(extensao):
-    for arquivo in os.listdir():
-        if arquivo.endswith(extensao):
-            print(arquivo)
-    if not any(arquivo.endswith(extensao) for arquivo in os.listdir()):
-        print(f"Nenhum arquivo com a extensão {extensao} encontrado.")
-try:
-    caminho = input("Digite o caminho do diretório: ")
-    os.chdir(caminho)
+def listarTodosArquivos(caminho):
+    if not os.listdir(caminho):
+        print("Não tem arquivos no diretório.")
+        return
 
-    while True:
-        print("1 - .txt\n2 - .word\n3 - .pdf\n4 - .excel\n")
-        escolha = int(input("Digite qual tipo de arquivo deseja listar: \n"))
-        match escolha:
-            case 1:
-                extensao = ".txt"
-                listarArquivos(extensao)
-            case 2:
-                extensao = ".docx"
-                listarArquivos(extensao)
-            case 3:
-                extensao = ".pdf"
-                listarArquivos(extensao)
-            case 4:
-                extensao = ".xlsx"
-                listarArquivos(extensao)
-            case _:
-                print("Opção inválida, tente novamente.")
+    for item in os.listdir(caminho):
+        caminho_completo = os.path.join(caminho, item)
+        
+        if os.path.isdir(caminho_completo):
+            print(f"[PASTA] {item}")
+        else:
+            print(f"[ARQUIVO] {item}")
+
+def listarArquivoPorExtensao(caminho, extensao):
+    arquivos_encontrados = [arq for arq in os.listdir(caminho) if arq.endswith(extensao)]
+    
+    if not arquivos_encontrados:
+        print("Não tem arquivos com essa extensão neste diretório.")
+        return
+        
+    for arquivo in arquivos_encontrados:
+        print(arquivo)
+
+try:
+    caminho = input("Digite o caminho para o diretório: ")
+    
+    if not os.path.exists(caminho):
+        raise FileNotFoundError
+        
+    print("\n1- Listar todos os arquivos\n2- Listar arquivos por extensão")
+    escolha = int(input("O que você deseja listar neste diretório? "))
+    
+    match escolha:
+        case 1:
+            listarTodosArquivos(caminho)
+        case 2:
+            extensao = input("Digite a extensão que deseja procurar (ex: .txt): ")
+            listarArquivoPorExtensao(caminho, extensao)
+        case _:
+            print("Digite uma das opções. tente novamente.")
+            
 except FileNotFoundError:
-    print("Diretório não encontrado.")
+    print("Diretório não encontrado!")
+except PermissionError:
+    print("Você não tem permissão para acessar essa pasta!")
 except ValueError:
-    print("Entrada inválida, por favor digite um número.")
+    print("Input inválido para a variável, tente novamente.")
 except Exception as e:
     print(f"Ocorreu um erro inesperado: {e}")
